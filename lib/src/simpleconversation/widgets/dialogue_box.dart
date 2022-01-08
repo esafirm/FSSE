fe3556df96a0a5fb77311d7cfaf9f92c49368b04
Esa Firman <esafirm21@gmail.com>
2022-01-08 14:36:22 +0700

    initial animated text

--- lib/src/simpleconversation/widgets/dialogue_box.dart
+++ lib/src/simpleconversation/widgets/dialogue_box.dart
@@ -16,9 +16,23 @@ class DialogueBox extends StatelessWidget {
         border: Border.all(color: Colors.blue, width: 2, style: BorderStyle.solid),
         borderRadius: const BorderRadius.all(Radius.circular(8)),
       ),
-      child: Text(
-        _text,
-        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Miki"),
+          TweenAnimationBuilder<int>(
+            key: Key(_text),
+            builder: (context, value, child) {
+              print("text: $_text value: $value");
+
+              return Text(_text.substring(0, value) + "_",
+                  style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500));
+            },
+            duration: const Duration(seconds: 2),
+            tween: IntTween(begin: 0, end: _text.length),
+          )
+        ],
       ),
     );
   }
